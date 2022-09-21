c7e93dd2a029d5f67e91e4f5504aaa4719754a9e
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-09-21 14:05:24 +0900

    [Silver II] Title: 좌표 압축, Time: 2028 ms, Memory: 156812 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,11 @@
+N = int(input())
+xlst = list(map(int, input().split()))
+
+index = sorted(list(set(xlst)))
+
+info = {}
+for i in range(len(index)):
+    info[index[i]] = i
+
+for k in range(N):
+    print(info[xlst[k]], end = " ")
