74c965103f7e56f5013b17a1c2d71f6ee9362a99
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-09-27 03:00:27 +0900

    [Silver I] Title: 나이트의 이동, Time: 2632 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -10,8 +10,8 @@ def BFS(x, y, cnt):
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            if x == ex and y == ey:
-                return print(cnt)
+            if x == ex and y == ey: ## 도착점이 꺼내졌다면
+                return print(cnt) ## 몇번만에 왔는지 출력한다.
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 queue.append((nx,ny,cnt+1))
                 visited[nx][ny] = 1              
@@ -21,5 +21,5 @@ for tc in range(int(input())):
     sy, sx = map(int, input().split())
     ey, ex = map(int, input().split())
     visited = [[0] * N for _ in range(N)]
-    visited[sx][sy] = 1
+    visited[sx][sy] = 1 ## 출발점으로 왔다갔다 안하기 위함.
     BFS(sx, sy, 0)
