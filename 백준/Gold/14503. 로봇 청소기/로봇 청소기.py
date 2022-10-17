2bb0d3a3ef76b593fb425a9a1012574b48b1b8c2
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-10-18 02:57:51 +0900

    [Gold V] Title: 로봇 청소기, Time: 112 ms, Memory: 114408 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,5 +1,5 @@
 dx = [-1, 0, 1, 0]  ## 북, 동, 남, 서
-dy = [0, 1, 0, -1] 
+dy = [0, 1, 0, -1]   # (문제 제시, 봐야 할 방향) ex(북, 서) (동, 북)
 
 def clean():
     global res
@@ -7,15 +7,14 @@ def clean():
     x, y = sx, sy
     while True:
         for _ in range(4):
-            d = (d+3)%4 ## 왼쪽 보고
+            d = (d+3)%4
             nx = x + dx[d]
             ny = y + dy[d]
-            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0:
-                if visited[nx][ny] == 0:##청소할 곳이라면
-                    visited[nx][ny] = 1  ## 청소하고
-                    x, y = nx, ny  ## 전진
-                    res += 1  ## 했어용~
-                    break  ## 다음 좌표에서 탐색해야 하니까 break
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0:  ##청소할 곳이라면
+                arr[nx][ny] = 3  ## 청소하고
+                x, y = nx, ny  ## 전진
+                res += 1  ## 했어용~
+                break  ## 다음 좌표에서 탐색해야 하니까 break
         else:  ## 네 방향 다 봤는데 청소 못했으면..
             if arr[x - dx[d]][y - dy[d]] != 1:  ## 후진 가능하면
                 x, y = x - dx[d], y - dy[d] # 후진하고
@@ -25,10 +24,9 @@ def clean():
 N, M = map(int, input().split())
 sx, sy, d = map(int, input().split())  # d==0북, d==1동, d==2남, d==3서
 arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
 res = 0
 if arr[sx][sy] == 0:
+    arr[sx][sy] = 3
     res += 1
-visited[sx][sy] = 1
 clean()
 print(res)
