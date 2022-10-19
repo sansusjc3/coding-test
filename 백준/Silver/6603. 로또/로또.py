45faf4193c4da8b5aeefbb859044d06dbef9136b
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-10-19 23:32:42 +0900

    Update 로또.py

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -7,13 +7,13 @@ def dfs(idx, n):
         dfs(i+1, n+1)
         res.pop()
 while True:
-    arr = list(map(int, input().split()))
-    if arr == [0]:
+    arr = list(map(int, input().split())) ##while안에서 번호를 받다가
+    if arr == [0]: ## 0이 들어오면 종료이므로...
         break 
-    k = arr[0]
+    k = arr[0] # 첫 인덱스가 개수이므로
     S = arr[1::]
-    res = []
-    result = []
+    res = [] ##조합을 완성시킬 리스트
+    result = [] ## 완성된 조합을 넣을 리스트
     dfs(0, 0)
     for i in (result):
         print(*i)
