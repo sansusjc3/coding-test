89260181963f5de201089ef62af398bc75c7965c
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-10-20 19:00:14 +0900

    [Gold IV] Title: 테트로미노, Time: 1328 ms, Memory: 124324 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,44 @@
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+def dfs(n, x, y, Sum):
+    global maxV
+    if n == 4:
+        if Sum > maxV:
+            maxV = Sum
+        return
+
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+            visited[nx][ny] = 1
+            dfs(n+1, nx, ny, Sum+arr[nx][ny])
+            visited[nx][ny] = 0
+
+def fuck(n, x, y, Sum):
+    global maxV
+    if n == 4:
+        if Sum > maxV:
+            maxV = Sum
+        return
+    if n >= 2:
+        x, y = sx, sy
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+            visited[nx][ny] = 1
+            fuck(n+1, nx, ny, Sum+arr[nx][ny])
+            visited[nx][ny] = 0
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+maxV = 0
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = 1
+        dfs(1, i, j, arr[i][j])
+        sx, sy = i, j
+        fuck(1, i, j, arr[i][j])
+        visited[i][j] = 0
+print(maxV)
