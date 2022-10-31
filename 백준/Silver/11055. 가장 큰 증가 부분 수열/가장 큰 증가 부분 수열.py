93c99bcc083ce1890f3f35c7394e4509c774e27e
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-11-01 02:08:41 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -7,7 +7,7 @@ for i in range(1, N):
     for j in range(i):
         if num[i] > num[j]: ## 증가 수열일 경우...
             dp[i] = max(dp[i], dp[j] + num[i])
-        else: ## 증가 수열이 아닐 때는 단일값을 넣어주어야 한다.(그렇지 않으면 초기값 0이 그대로..)
-            dp[i] = max(dp[i], num[i])
+        else: ## 증가 수열이 아닐 때는 단일값과 현재까지의 dp(그 전 증가수열이었을 인덱스 때 넣어둔 값)를 비교한다.
+            dp[i] = max(dp[i], num[i]) ## 아무 비교도 없을 시 초기 할당한 0 이 그대로..
 
 print(max(dp))
