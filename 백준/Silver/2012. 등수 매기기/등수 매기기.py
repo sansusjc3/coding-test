acfe5f4e993ed8a6d303dd8b432380e01577c914
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-11-06 17:05:38 +0900

    Update 등수 매기기.py

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -1,9 +1,9 @@
 N = int(input())
-result = [0]
+result = [0] ## 인덱스와 순위를 맞춰주기 위해 맨 앞에 하나 추가
 for _ in range(N):
     result.append(int(input()))
 result.sort()
 res = 0
-for i in range(1, N+1):
+for i in range(1, N+1): ## 정렬한 배열을 돌면서 인덱스 - 순위의 절대값
     res += abs(i-result[i])
 print(res)
