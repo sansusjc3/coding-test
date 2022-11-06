19f8a6a1ac6a586390c9ef0cfeee75759a244159
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2022-11-06 17:22:45 +0900

    Update 주식.py

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,11 +1,11 @@
 for _ in range(int(input())):
     N = int(input())
     arr = list(map(int, input().split()))
-    cri = arr[-1] 
+    cri = arr[-1]
     res = 0
     for i in range(N-2, -1, -1):
         if arr[i] >= cri:
             cri = arr[i]
         else:
             res += cri-arr[i]
-    print(res)
+## 역순으로 돌면서 기준 값보다 큰 값이 나오면 갱신, 작은 값이 나오면 결과에 더해준다.
