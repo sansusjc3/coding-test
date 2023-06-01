40d166c24f697abb662053ca960e0db41bf5bea6
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2023-06-01 23:16:34 +0900

    [Gold V] Title: 탑, Time: 432 ms, Memory: 207092 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,20 +1,19 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
 N = int(input())
 tower = [0] + list(map(int, input().split()))
 stack = deque()
 res = deque()
 
-for i in range(1, N+1):
+for idx, height in enumerate(tower[1:], start = 1):
     while stack:
-        if stack[-1][1] > tower[i]:
+        if stack[-1][1] > height:
             res.append(stack[-1][0])
             break
         else:
             stack.pop()
     else:
         res.append(0)
-    stack.append([i, tower[i]])
+    stack.append((idx, height))
 print(*res)
