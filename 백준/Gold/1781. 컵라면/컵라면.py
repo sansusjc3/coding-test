7ea7f7537194f916edf861736882b9089c2f8d70
Jo Seung Hyeon <109322161+sansusjc3@users.noreply.github.com>
2023-06-03 00:39:21 +0900

    [Gold II] Title: 컵라면, Time: 340 ms, Memory: 139260 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.py
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -2,15 +2,17 @@ import sys
 import heapq
 
 N = int(sys.stdin.readline())
-arr = []
+arr = [[] for _ in range(N+1)]
+q = []
 res = 0
+
 for _ in range(N):
     dead, cup = map(int, sys.stdin.readline().split())
-    arr.append((dead, cup))
-arr.sort()
-q = []
-for problem in arr:
-    heapq.heappush(q, problem[1])
-    if problem[0] < len(q):
-        heapq.heappop(q)
-print(sum(q))
+    arr[dead].append(cup)
+
+for idx in range(N, 0, -1):
+    for cup in arr[idx]:
+        heapq.heappush(q, -cup)
+    if q:
+        res -= heapq.heappop(q)
+print(res)
